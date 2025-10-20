a2c1ae58a5ebb19a0fb3340745807719d9bced60
Monish Nallagondalla <nsmonish@gmail.com>
2025-10-20 23:32:29 +0530

    get embedding update and create db

--- image/create_db.py
+++ image/create_db.py
@@ -7,3 +7,69 @@ from langchain.schema.document import Document
 from langchain.vectorstores.chroma import Chroma
 
 from src.rag_app.get_embeddings import get_embedding_function
+
+
+CHROMA_PATH = "src/data/chroma"
+DATA_SOURCE_PATH = "src/data/source"
+
+
+def main():
+
+    # Check if the database should be cleared (using the --clear flag).
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--reset", action="store_true", help="Reset the database.")
+    args = parser.parse_args()
+    if args.reset:
+        print("✨ Clearing Database")
+        clear_database()
+
+    # Create (or update) the data store.
+    documents = load_documents()
+    chunks = split_documents(documents)
+    add_to_chroma(chunks)
+
+
+def load_documents():
+    document_loader = PyPDFDirectoryLoader(DATA_SOURCE_PATH)
+    return document_loader.load()
+
+
+def split_documents(documents: list[Document]):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=600,
+        chunk_overlap=120,
+        length_function=len,
+        is_separator_regex=False,
+    )
+    return text_splitter.split_documents(documents)
+
+
+def add_to_chroma(chunks: list[Document]):
+    # Load the existing database.
+    db = Chroma(
+        persist_directory=CHROMA_PATH, embedding_function=get_embedding_function()
+    )
+
+    # Calculate Page IDs.
+    chunks_with_ids = calculate_chunk_ids(chunks)
+    for chunk in chunks:
+        print(f"Chunk Page Sample: {chunk.metadata['id']}\n{chunk.page_content}\n\n")
+
+    # Add or Update the documents.
+    existing_items = db.get(include=[])  # IDs are always included by default
+    existing_ids = set(existing_items["ids"])
+    print(f"Number of existing documents in DB: {len(existing_ids)}")
+
+    # Only add documents that don't exist in the DB.
+    new_chunks = []
+    for chunk in chunks_with_ids:
+        if chunk.metadata["id"] not in existing_ids:
+            new_chunks.append(chunk)
+
+    if len(new_chunks):
+        print(f"👉 Adding new documents: {len(new_chunks)}")
+        new_chunk_ids = [chunk.metadata["id"] for chunk in new_chunks]
+        db.add_documents(new_chunks, ids=new_chunk_ids)
+    else:
+        print("✅ No new documents to add")
+
