5cdfd7362158f43471aa199e9ea9898a56a4f900
Monish Nallagondalla <nsmonish@gmail.com>
2025-10-23 23:33:48 +0530

    updated get chroma db

--- image/src/rag_app/get_chroma_db.py
+++ image/src/rag_app/get_chroma_db.py
@@ -29,3 +29,25 @@ def get_chroma_db():
         print(f"✅ Init ChromaDB {CHROMA_DB_INSTANCE} from {get_runtime_chroma_path()}")
 
     return CHROMA_DB_INSTANCE
+
+
+def copy_chroma_to_tmp():
+    dst_chroma_path = get_runtime_chroma_path()
+
+    if not os.path.exists(dst_chroma_path):
+        os.makedirs(dst_chroma_path)
+
+    tmp_contents = os.listdir(dst_chroma_path)
+    if len(tmp_contents) == 0:
+        print(f"Copying ChromaDB from {CHROMA_PATH} to {dst_chroma_path}")
+        os.makedirs(dst_chroma_path, exist_ok=True)
+        shutil.copytree(CHROMA_PATH, dst_chroma_path, dirs_exist_ok=True)
+    else:
+        print(f"✅ ChromaDB already exists in {dst_chroma_path}")
+
+
+def get_runtime_chroma_path():
+    if IS_USING_IMAGE_RUNTIME:
+        return f"/tmp/{CHROMA_PATH}"
+    else:
+        return CHROMA_PATH
